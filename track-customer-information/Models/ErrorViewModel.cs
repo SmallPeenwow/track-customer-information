58bcbde812a9b40053b6ac2849085f1cca356b34
SmallPeenwow <bowdencamryn@gmail.com>
2024-03-18 22:35:38 +0200

    Add project files.

---
+++ track-customer-information/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace track_customer_information.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
